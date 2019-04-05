596f142bf9d91b7a47708e77ad65fb04a7b48756
ksknight <korvette_00Whotmail.com>
2019-04-05 13:22:03 -0600

    update files to deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require("express");
-var path = require("path");
-
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "mySQL_DATABASE"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+
+//   console.log("connected as id " + connection.threadId);
+// });
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
