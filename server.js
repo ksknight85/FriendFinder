3c90013a20abc7cc1be08b369c29a5a2bf4905d5
ksknight <korvette_00Whotmail.com>
2019-04-08 21:28:59 -0600

    update

--- server.js
+++ server.js
@@ -2,36 +2,14 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-var path = require("path");
-var mysql = require("mysql");
+require("./app/routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
 
-app.get("/", function(req, res) {
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-
-  });
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   user: "root",
-//   password: "root",
-//   database: "mySQL_DATABASE"
-// });
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-
-//   console.log("connected as id " + connection.threadId);
-// });
-
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
